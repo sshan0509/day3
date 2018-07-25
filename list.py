129c12bb094a3bc1eab14b427a354cb8f67dd326
sshan0509 <sshan0509@naver.com>
2018-07-25 16:35:28 +0900

    실습파일 추가

---
+++ list.py
@@ -0,0 +1,9 @@
+fruits = ['apple', 'banana', 'watermelon', 'orange']
+print(fruits)
+fruits.append('melon')
+print(fruits)
+print(fruits[0])
+del fruits[0]
+print(fruits)
+fruits.append('apple'[0:5])
+print(fruits)
---
+++ string.py
@@ -0,0 +1,7 @@
+name = "Nick"
+city = 'Seoul'
+print(name)
+print(city)
+print(name, city)
+print(name + " " + city)
+print("Hello, " + name)
